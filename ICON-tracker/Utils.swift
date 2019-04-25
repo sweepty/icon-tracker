b5eb9ad60803f284e349c37cd36399607cad2ad5
sweepty <adie0423@gmail.com>
2019-04-25 17:11:31 +0900

    ✨Add hex string converter functions

--- ICON-tracker/Utils.swift
+++ ICON-tracker/Utils.swift
@@ -15,3 +15,37 @@ extension BigUInt {
         return self / BigUInt(1000000000000000000)
     }
 }
+
+extension String {
+    /// Convert hex String to Int
+    public func hextoInt() -> Int {
+        return Int(self.prefix0xRemoved(), radix: 16) ?? 0
+    }
+    
+    /// Convert hex String to Date
+    public func hextoDate() -> Date? {
+        guard let sub = Int(self.prefix0xRemoved(), radix: 16) else {
+            return nil
+        }
+        return Date(timeIntervalSince1970: Double(sub) / 1000000.0)
+    }
+}
+
+extension Data {
+    // hex String to data
+    init?(hexString: String) {
+        let len = hexString.count / 2
+        var data = Data(capacity: len)
+        for i in 0..<len {
+            let j = hexString.index(hexString.startIndex, offsetBy: i*2)
+            let k = hexString.index(j, offsetBy: 2)
+            let bytes = hexString[j..<k]
+            if var num = UInt8(bytes, radix: 16) {
+                data.append(&num, count: 1)
+            } else {
+                return nil
+            }
+        }
+        self = data
+    }
+}
