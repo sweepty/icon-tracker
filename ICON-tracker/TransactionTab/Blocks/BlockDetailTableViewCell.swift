39e44e878307f2a192516acb0465562c8deebff6
Seungyeon Lee <adie0423@gmail.com>
2019-05-08 00:27:23 +0900

    🚧 Update UI - BlockDetailView and Transaction Tab

--- ICON-tracker/TransactionTab/Blocks/BlockDetailTableViewCell.swift
+++ ICON-tracker/TransactionTab/Blocks/BlockDetailTableViewCell.swift
@@ -17,6 +17,11 @@ class BlockDetailTableViewCell: UITableViewCell {
     @IBOutlet weak var toLabel: UILabel!
     @IBOutlet weak var amountLabel: UILabel!
     
+    @IBOutlet weak var txHashTitleView: UIView!
+    @IBOutlet weak var toTtitleView: UIView!
+    @IBOutlet weak var fromTitleView: UIView!
+    @IBOutlet weak var icxView: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
